fdfa5dafc92d1fe5b2d351713ade141728136399
cleobuck <cleo.buck@gmail.com>
2019-09-10 09:33:23 +0200

    user database reset

--- connection.php
+++ connection.php
@@ -4,8 +4,8 @@
 try
 {
 	// On se connecte à MySQL
-    // $bd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_13669c3d17eed1b;charset=utf8', 'b8355276f895fc', 'd7ae0ad2');
-    $bd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8', 'phpmyadmin', 'user');
+    $bd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_13669c3d17eed1b;charset=utf8', 'b8355276f895fc', 'd7ae0ad2');
+    // $bd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8', 'phpmyadmin', 'user');
 }
 catch(Exception $e)
 {
