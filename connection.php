a947351f38ae2d73616b54ed8bc7775aedc13cb0
cleobuck <cleo.buck@gmail.com>
2019-09-10 09:31:39 +0200

    user database reset

--- connection.php
+++ connection.php
@@ -4,8 +4,8 @@
 try
 {
 	// On se connecte à MySQL
-    $bd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_13669c3d17eed1b;charset=utf8', 'b8355276f895fc', 'd7ae0ad2');
-    // $bd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8', 'phpmyadmin', 'user');
+    // $bd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_13669c3d17eed1b;charset=utf8', 'b8355276f895fc', 'd7ae0ad2');
+    $bd = new PDO('mysql:host=localhost;dbname=weatherapp;charset=utf8', 'phpmyadmin', 'user');
 }
 catch(Exception $e)
 {
@@ -16,6 +16,9 @@ catch(Exception $e)
 
 //creation de la table users
 
+$sql=$bed->prepare("DROP TABLE  users");
+
+$sql->execute();
 
 $users = "CREATE TABLE IF NOT EXISTS users
     (email varchar(25), pass varchar(500))";
